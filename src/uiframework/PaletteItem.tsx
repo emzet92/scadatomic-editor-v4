8163f4a95652f5ace28db1643eb255e68d4a0f38
mateusz <mzalewski1992@gmail.com>
2026-06-15 22:38:07 +0200

    prettier component pallette

--- src/uiframework/PaletteItem.tsx
+++ src/uiframework/PaletteItem.tsx
@@ -1,11 +1,11 @@
 // ComponentPalette.tsx
 
 import { useEditorStore } from "./editor-store";
-
-type PaletteItem = {
-  type: keyof typeof defaults;
-  label: string;
-};
+import {
+  Box,
+  Type,
+  RectangleHorizontal,
+} from "lucide-react";
 
 const defaults = {
   Container: {
@@ -25,18 +25,30 @@ const defaults = {
   },
 };
 
-const items: PaletteItem[] = [
+const items = [
   {
-    type: "Container",
+    type: "Container" as const,
     label: "Container",
+    description:
+      "Layout container",
+    icon: Box,
   },
+
   {
-    type: "Text",
+    type: "Text" as const,
     label: "Text",
+    description:
+      "Static text",
+    icon: Type,
   },
+
   {
-    type: "Button",
+    type: "Button" as const,
     label: "Button",
+    description:
+      "User action",
+    icon:
+      RectangleHorizontal,
   },
 ];
 
@@ -47,31 +59,112 @@ export function ComponentPalette() {
     );
 
   return (
-    <div data-editor-ignore>
-      <h3>Components</h3>
-
-      {items.map((item) => (
+    <div
+      data-editor-ignore
+      className="space-y-3"
+    >
+      <div>
         <div
-          key={item.type}
-          onPointerDown={() => {
-            startComponentDrag({
-              type: item.type,
-              props: defaults[item.type],
-            });
-          }}
-          style={{
-            padding: 8,
-            marginBottom: 6,
-            border: "1px solid #ccc",
-            borderRadius: 4,
-            cursor: "grab",
-            userSelect: "none",
-            width: 150,
-          }}
+          className="
+            text-xs
+            uppercase
+            tracking-wide
+            font-semibold
+            text-zinc-500
+          "
         >
-          {item.label}
+          Components
         </div>
-      ))}
+      </div>
+
+      {items.map((item) => {
+        const Icon =
+          item.icon;
+
+        return (
+          <button
+            key={item.type}
+            data-editor-ignore
+            onPointerDown={() => {
+              startComponentDrag(
+                {
+                  type: item.type,
+                  props:
+                    defaults[
+                    item.type
+                    ],
+                }
+              );
+            }}
+            className="
+              w-full
+              p-3
+
+              rounded-xl
+              border
+              border-zinc-200
+
+              bg-white
+
+              hover:border-sky-300
+              hover:bg-sky-50
+
+              transition-all
+
+              flex
+              items-start
+              gap-3
+
+              text-left
+              cursor-grab
+              select-none
+            "
+          >
+            <div
+              className="
+                h-9
+                w-9
+
+                rounded-lg
+
+                bg-sky-100
+                text-sky-600
+
+                flex
+                items-center
+                justify-center
+
+                shrink-0
+              "
+            >
+              <Icon size={18} />
+            </div>
+
+            <div className="min-w-0">
+              <div
+                className="
+                  text-sm
+                  font-medium
+                  text-zinc-900
+                "
+              >
+                {item.label}
+              </div>
+
+              <div
+                className="
+                  text-xs
+                  text-zinc-500
+                "
+              >
+                {
+                  item.description
+                }
+              </div>
+            </div>
+          </button>
+        );
+      })}
     </div>
   );
 }
