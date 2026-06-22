f9aa1149909ad1647cdc87e835f7d84110209d90
mateusz <mzalewski1992@gmail.com>
2026-06-22 20:59:03 +0200

    REFACTOR-first version of runtime component

--- src/uiframework/components/RuntimeButton.tsx
+++ src/uiframework/components/RuntimeButton.tsx
@@ -1,9 +1,29 @@
 // components/RuntimeButton.tsx
 
+import { getWs } from "../websocket";
 import { Button } from "./Button";
 
-export function RuntimeButton(props: any) {
+export function RuntimeButton({
+  id,
+  onClickEvent,
+  ...props
+}) {
   return (
-    <Button {...props} />
+    <Button
+      {...props}
+      onClick={
+        onClickEvent
+          ? () => {
+              getWs().send(
+                JSON.stringify({
+                  event:
+                    onClickEvent,
+                  nodeId: id,
+                })
+              );
+            }
+          : undefined
+      }
+    />
   );
 }
