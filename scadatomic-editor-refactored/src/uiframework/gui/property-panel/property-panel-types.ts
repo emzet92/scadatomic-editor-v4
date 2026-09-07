3f5afd5d0ecab732fe5f4c80192efade726bdcc1
mateusz <mzalewski1992@gmail.com>
2026-09-07 23:49:02 +0200

    cleanup

--- scadatomic-editor-refactored/src/uiframework/gui/property-panel/property-panel-types.ts
+++
@@ -1,9 +0,0 @@
-import type { UiNode } from "../../core/document";
-
-export type { UiNode };
-export type PropEntry = [string, unknown];
-
-export type UpdateNode = (
-  nodeId: string,
-  updater: (node: UiNode) => UiNode
-) => void;
--- scadatomic-editor-refactored/src/uiframework/registry/runtime-registry.ts
+++
@@ -1,9 +0,0 @@
-import type { ComponentRegistry } from "./editor-registry";
-import { componentDefinitions } from "./component-definitions";
-
-export const runtimeRegistry: ComponentRegistry = Object.fromEntries(
-  Object.values(componentDefinitions).map((definition) => [
-    definition.type,
-    definition.runtime,
-  ])
-);
